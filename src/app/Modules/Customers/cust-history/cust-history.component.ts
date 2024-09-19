0f192cf2b7688bcb223d03cb00f1f8c8768f864e
leya <smac4510@gmail.com>
2024-09-20 07:45:51 +0800

    ThirdCommit

--- src/app/Modules/Customers/cust-history/cust-history.component.ts
+++ src/app/Modules/Customers/cust-history/cust-history.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cust-history',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './cust-history.component.html',
   styleUrl: './cust-history.component.css'
 })
 export class CustHistoryComponent {
+  today: any;
+
   constructor(
     private route: Router
-  ){}
+  ){
+    this.today = ''; // Initialize today
+  }
   back(){
     this.route.navigate(['./mainpage/customers/custpage/custlist'])
+  }
+
+
+  ngOnInit(): void {
+    const date = new Date();
+    this.today = date.toISOString().split('T')[0];
   }
 }
--- src/app/Modules/Home/home-page/home-page.component.ts
+++ src/app/Modules/Home/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReleaseComponent } from '../release/release.component';
 import { ReceivingComponent } from '../receiving/receiving.component';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-home-page',
@@ -9,6 +10,42 @@ import { ReceivingComponent } from '../receiving/receiving.component';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  ngOnInit(): void {
+    this.initializeNavigation();
+  }
 
+  private initializeNavigation(): void {
+    $('nav').each(function() {
+      let $active: JQuery<HTMLElement>, $content: JQuery<HTMLElement>;
+      const $links: JQuery<HTMLElement> = $(this).find('a');
+
+      $active = $($links.filter(`[href="${location.hash}"]`)[0] || $links[0]);
+      $active.addClass('active');
+
+      const activeAnchor = $active[0] as HTMLAnchorElement;
+      $content = $(activeAnchor.hash);
+
+      $links.not($active).each(function() {
+
+        const linkElement = this as HTMLAnchorElement;
+        $(linkElement.hash).hide();
+      });
+
+      $(this).on('click', 'a', function(e: JQuery.Event) {
+        e.preventDefault();
+
+        $active.removeClass('active');
+        $content.hide();
+
+        const anchorElement = this as HTMLAnchorElement;
+
+        $active = $(this);
+        $content = $(anchorElement.hash);
+
+        $active.addClass('active');
+        $content.show();
+      });
+    });
+  }
 }
